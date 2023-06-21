26d416a504aac8c0c7b80df3475c5ce3733f8630
Ben Kotvis <ben.kotvis@insight.com>
2023-06-21 13:01:51 +0000

    Adding a page

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,32 @@
 
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./i18n";
 import initIcons from "./services/IconService/IconService";
 
+import Search from "./Search";
 import App from "./App";
 import "./index.scss";
 
 initIcons();
 
+
+export default function MainApp() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/graph"><App /></Route>
+        <Route path="/"><Search /></Route>
+      </Switch>
+    </Router>
+  );
+}
+
+
 ReactDOM.render(
   <Suspense fallback={<>Loading...</>}>
-    <App />
+    <MainApp />
   </Suspense>,
   document.getElementById("root")
 );
